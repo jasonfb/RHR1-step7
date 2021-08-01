840ee7f11a41cad9688f549860f33de61280dba6
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2021-08-01 19:57:54 -0400

    setup capybara

---
+++ spec/features/math_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "take a quiz", type: :feature do
+  it "asks me to add to two numbers" do
+    visit '/'
+    expect(page).to have_content 'Hello world'
+
+    # expect(page).to have_content 'What is 5+2'
+  end
+end
